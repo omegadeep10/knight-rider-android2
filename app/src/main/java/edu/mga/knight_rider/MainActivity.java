73774ad6dcc05c109dde9ba19f92998a712e8d71
Deep Patel <omegadeep10@gmail.com>
2018-03-13 19:50:28 -0400

    Setup event handler for touching the menu icon so that it triggers sidebar

--- app/src/main/java/edu/mga/knight_rider/MainActivity.java
+++ app/src/main/java/edu/mga/knight_rider/MainActivity.java
@@ -1,21 +1,38 @@
 package edu.mga.knight_rider;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout sidebar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialize and create our toolbar and hamburger menu icon
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        sidebar = findViewById(R.id.drawer_layout);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                sidebar.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
