fdff45382ce737fc7c946640a1d9b0d29b18ce0b
Deep Patel <omegadeep10@gmail.com>
2018-03-12 21:35:44 -0400

    Add working navigation and hamburger menu. Still need to wire up touch events

--- app/src/main/java/edu/mga/knight_rider/MainActivity.java
+++ app/src/main/java/edu/mga/knight_rider/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.mga.knight_rider;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
     }
 }
