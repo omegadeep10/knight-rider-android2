b6babb3855160587e0a8810709629886561f2e99
Deep Patel <omegadeep10@gmail.com>
2018-03-16 22:18:53 -0400

    Add basic login button event handler

--- app/src/main/java/edu/mga/knight_rider/LoginActivity.java
+++ app/src/main/java/edu/mga/knight_rider/LoginActivity.java
@@ -1,13 +1,31 @@
 package edu.mga.knight_rider;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText usernameInput;
+    private EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameInput = (EditText)findViewById(R.id.loginUsername);
+        passwordInput = (EditText)findViewById(R.id.loginPassword);
+
+        RelativeLayout loginButton = (RelativeLayout)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Hey! You've activated my trap card!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
