3c19383fd978259cd1d8671f14edec8071bb42dc
omegadeep10 <omegadeep10@gmail.com>
2018-03-17 18:21:55 -0400

    Add login button loading indicator on click

--- app/src/main/java/edu/mga/knight_rider/LoginActivity.java
+++ app/src/main/java/edu/mga/knight_rider/LoginActivity.java
@@ -1,31 +1,45 @@
 package edu.mga.knight_rider;
 
+import android.os.Handler;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText usernameInput;
     private EditText passwordInput;
+    private ProgressBar loginProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        usernameInput = (EditText)findViewById(R.id.loginUsername);
-        passwordInput = (EditText)findViewById(R.id.loginPassword);
+        usernameInput = findViewById(R.id.loginUsername);
+        passwordInput = findViewById(R.id.loginPassword);
+        loginProgressBar = findViewById(R.id.progressBar);
+    }
 
-        RelativeLayout loginButton = (RelativeLayout)findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+    public void Login(View view) {
+        final Button loginButton = (Button) view; // Get our button
+        loginButton.setEnabled(false);      // Disable button while loading view/
+        loginButton.setText("");
+        loginProgressBar.setVisibility(View.VISIBLE);
+
+        (new Handler()).postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getBaseContext(), "Hey! You've activated my trap card!", Toast.LENGTH_LONG).show();
+            public void run() {
+                Toast.makeText(getBaseContext(), "This button should log you in.", Toast.LENGTH_LONG).show();
+                loginProgressBar.setVisibility(View.GONE);
+                loginButton.setEnabled(true);
+                loginButton.setText("LOG IN");
             }
-        });
+        }, 5000);
     }
 }
