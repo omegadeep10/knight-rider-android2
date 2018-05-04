9339a83bfaf78324d3a127f4d0d917704996b064
astrogirl133 <fire-fox-9@hotmail.com>
2018-05-04 03:03:31 -0400

    Added firebase stuff

--- app/src/main/java/edu/mga/knight_rider/MyFirebaseInstanceIDService.java
+++ app/src/main/java/edu/mga/knight_rider/MyFirebaseInstanceIDService.java
@@ -1,13 +1,23 @@
 package edu.mga.knight_rider;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MyFirebaseInstanceIDService extends Service {
+    private static final String TAG = "FirebaseIDService";
 
-    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+
     public void onTokenRefresh() {
         // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        String refreshedToken = FirebaseInstanceIdseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
 
         // If you want to send messages to this application instance or
--- app/src/main/java/edu/mga/knight_rider/MyFirebaseMessagingService.java
+++ app/src/main/java/edu/mga/knight_rider/MyFirebaseMessagingService.java
@@ -1,6 +1,28 @@
 package edu.mga.knight_rider;
 
 import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends Service {
+
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    private static final String TAG = "FCM Service";
+
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+
 }
